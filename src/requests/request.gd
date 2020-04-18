4ec48aa82c4078e69ae938b6729ab7f169e49548
DeinFreund <flyck@ethz.ch>
2020-04-18 21:40:03 +0200

    fix bugs

--- src/requests/request.gd
+++ src/requests/request.gd
@@ -2,8 +2,6 @@ extends Node
 
 class_name Request
 
-var Self = load("request.gd")
-
 signal request_fulfilled(request)
 
 var id : int
@@ -13,19 +11,23 @@ func _init(request_id, request_type):
 	self.id = request_id
 	self.type = request_type
 	
+	
+var _children_left = 0
+
 func process():
 	var ret = []
 	for t in type.requirements:
-		ret.append(Self.new(id, t))
-		ret[-1].connect("request_fulfilled", self, "child_fulfilled")
-	if len(ret) == 0:
+		for i in t.count:
+			ret.append(get_script().new(id, t.type))
+			ret[-1].connect("request_fulfilled", self, "child_fulfilled")
+			_children_left += 1
+	if _children_left == 0:
 		emit_signal("request_fulfilled", self)
 	return ret
 	
-var _children_fulfilled = 0
 
 func child_fulfilled(request):
-	_children_fulfilled += 1
-	assert(_children_fulfilled <= len(type.requirements))
-	if _children_fulfilled == len(type.requirements):
+	_children_left -= 1
+	assert(_children_left >= 0)
+	if _children_left == 0:
 		emit_signal("request_fulfilled", self)
--- src/services/service.gd
+++ src/services/service.gd
@@ -8,49 +8,52 @@ var running = false
 var cycles_used = 0
 
 func _init(type_):
-    type = type_
-    for rtype in type.inputs.keys():
-        request_queue[rtype] = []
+	type = type_
+	for rtype in type.inputs.keys():
+		request_queue[rtype] = []
 
 func can_handle(request):
-    if running:
-        return false
-    var rtype = request.type
-    return request_queue.has(rtype) and len(request_queue[rtype]) < type.inputs[rtype]
+	if running:
+		return false
+	var rtype = request.type
+	return request_queue.has(rtype) and len(request_queue[rtype]) < type.inputs[rtype]
 
 func handle_request(request):
-    if can_handle(request):
-        request_queue[request.type].append(request)
+	if can_handle(request):
+		request_queue[request.type].append(request)
 
 func can_start():
-    if running:
-        return false
-    for rtype in request_queue.keys():
-        if len(request_queue[rtype]) < type.inputs[rtype]:
-            return false
-    return true
+	if running:
+		return false
+	for rtype in request_queue.keys():
+		if len(request_queue[rtype]) < type.inputs[rtype]:
+			return false
+	return true
 
 func start():
-    if not can_start():
-        return
-    running = true
-    cycles_used = 0
+	if not can_start():
+		return
+	running = true
+	cycles_used = 0
 
 func is_running():
-    return running and not is_finished()
+	return running and not is_finished()
 
 func is_finished():
-    return running and cycles_used >= type.cpu
+	return running and cycles_used >= type.cpu
 
 func get_results():
-    var res = []
-    for rtype in request_queue.keys():
-        for request in request_queue[rtype]:
-            var new_requests = request.process()
-            for req in new_requests:
-                res.append(req)
-    return res
+	var res = []
+	for rtype in request_queue.keys():
+		for request in request_queue[rtype]:
+			var new_requests = request.process()
+			for req in new_requests:
+				res.append(req)
+	running = false
+	for rtype in type.inputs.keys():
+		request_queue[rtype] = []
+	return res
 
 func cycle():
-    if running:
-        cycles_used += 1
+	if running:
+		cycles_used += 1
